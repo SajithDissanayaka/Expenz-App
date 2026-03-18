9214945d14d95522b3ad66176a407b1f67b6c493
SajithDissanayaka <hdspdissanayaka@gmail.com>
2026-03-18 11:30:27 +0530

    Form and form validation

--- lib/screen/user_Data_screen.dart
+++ lib/screen/user_Data_screen.dart
@@ -1,3 +1,7 @@
+import 'package:expenz_app/constance/colors.dart';
+import 'package:expenz_app/constance/constance.dart';
+import 'package:expenz_app/widget/custome_button.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class UserDataScreen extends StatefulWidget {
@@ -8,10 +12,193 @@ class UserDataScreen extends StatefulWidget {
 }
 
 class _UserDataScreenState extends State<UserDataScreen> {
+  //for the check box
+  bool _rememberMe = false;
+  
+//form key for the validation
+final _formKey = GlobalKey<FormState>();
+
+//controller for the text from field
+
+final TextEditingController _userNameController = TextEditingController();
+final TextEditingController _emailController = TextEditingController();
+final TextEditingController _passwordController = TextEditingController();
+final TextEditingController _confirmpasswordController = TextEditingController();
+
+@override
+  void dispose() {
+    _userNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmpasswordController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("User data"),),
+      body: SingleChildScrollView(
+        child: SafeArea(child: 
+        Padding(
+          padding: const EdgeInsets.all(KDefaultPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Enter Your \nPersonal Details",style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+
+              //form import
+
+              Form(child: Column(
+                key: _formKey,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //form field for the user name
+                  TextFormField(
+                    controller: _userNameController,
+                    validator: (value) {
+                      //check wheather the user enteed a valid username
+                      if(value!.isEmpty){
+                        return "Please enter valid name";
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      contentPadding: EdgeInsets.all(20), 
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  // form fild for the email 
+                  TextFormField(
+                    controller: _emailController,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Please enter valid value";
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      contentPadding: EdgeInsets.all(20), 
+                    ),
+                  ),
+
+                    SizedBox(
+                    height: 20,
+                  ),
+
+                  //text fild for password
+                  TextFormField(
+                    controller: _passwordController,
+                    validator: (value) {
+                      if(value!.isEmpty){
+                        return "please enter the valid password";
+                      }
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      contentPadding: EdgeInsets.all(20), 
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  // user confirm password
+                  TextFormField(
+                    controller: _confirmpasswordController,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Please enter the same password";
+                      }
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Confirm Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      contentPadding: EdgeInsets.all(20), 
+                    ),
+                  ), 
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  //remember me for the next time
+                  Row(
+                    children: [
+                      Text("Remenber me next time",style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: kGrey,
+                      ),),
+                      Expanded(child: CheckboxListTile(
+                        activeColor: kMainColor,
+                        value: _rememberMe, onChanged: (value){
+                          setState(() {
+                            _rememberMe = value!;
+
+                          }
+                          );
+                        },
+                        ),
+                      
+                        ),
+                       
+                    ],
+                  ),
+                   SizedBox(
+                          height: 20,
+                        ),
+
+                        //submit button
+                        GestureDetector(
+                          onTap: (){
+                            if(_formKey.currentState!.validate()){
+
+                              //form is valid process data
+                              String username = _userNameController.text;
+                              String email = _emailController.text;
+                              String password = _passwordController.text;
+                              String ConfirmPassword = _confirmpasswordController.text;
+
+                              print("$username $email $password $ConfirmPassword");
+
+                            }
+                          },
+                          child: CustomeButton(
+                            buttonName: "Next", 
+                            buttonColor: kMainColor),
+                        )
+                ],
+              ))
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
